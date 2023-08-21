3ddca5144865505ac69ec26b92088e74917bc17e
Yinpu Li <yinpuli@icloud.com>
2023-08-20 19:14:42 -0700

    Update notebooks.

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from scipy.io import loadmat
 from datetime import datetime, timedelta
-
+import joblib
 import os
 import sys
 
@@ -12,6 +12,72 @@ sys.path.append(root_dir)
 
 
 from utils.constants import *
+
+
+
+# def save_model(file_name, model, model_info=None):
+#     """
+#     Save a model and its associated info (if provided) to a file using joblib.
+
+#     Parameters:
+#     file_name (str): The name of the file to save the model.
+#     model (object): The model object to be saved.
+#     model_info (dict, optional): Additional information about the model (default: None).
+
+#     # Usage example
+#     best_model = xgb.XGBRegressor(objective='reg:squarederror')
+#     best_params = {'max_depth': 5, 'learning_rate': 0.01, 'n_estimators': 100}
+#     best_score = 0.85
+#     model_info = {'best_params': best_params, 'best_score': best_score}
+
+#     save_model('best_xgb_model.joblib', best_model, model_info)
+
+#     """
+#     directory = os.path.dirname(file_name)
+#     os.makedirs(directory, exist_ok=True)
+#     joblib.dump((model, model_info), file_name)
+
+
+def save_model(file_name, model, model_info=None):
+    """
+    Save a model and its associated info (if provided) to a file using joblib.
+
+    Parameters:
+    file_name (str): The name of the file to save the model.
+    model (object): The model object to be saved.
+    model_info (dict, optional): Additional information about the model (default: None).
+    """
+    directory = os.path.dirname(file_name)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+    elif os.path.exists(file_name):
+        raise ValueError(f"File '{file_name}' already exists. Please provide a new file name.")
+
+    joblib.dump((model, model_info), file_name)
+
+
+
+def load_model(file_name):
+    """
+    Load a saved model and its associated info using joblib.
+
+    Parameters:
+    file_name (str): The name of the file containing the saved model.
+
+    Returns:
+    model: The loaded model object.
+    model_info: Additional information about the model (if available).
+    
+    # Load the model and its info
+    loaded_model, loaded_model_info = load_model(best_xgb_file)
+    """
+    return joblib.load(file_name)
+
+
+
+# Now you can use the loaded_model and loaded_model_info variables
+# to access the loaded XGBoost model and its associated info.
+
 
 
 def get_train_dates_col(start_date: datetime = datetime.strptime(train_start, "%Y-%m-%d"),
@@ -56,65 +122,6 @@ def get_test_dates_col(end_date: datetime = datetime.strptime(test_end, "%Y-%m-%
     date_strings = [(start_date + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(num_days)]
     return date_strings
 
-
-# def add_temporal_spatial_cols(
-#         loaded_data
-#         , start_date=None
-#         , end_date=None
-#         , train_num_days=None
-#         , test_num_days=None
-# ):
-#     """
-#     Adding new feature columns to train/test data sets,
-#     which contain time (contiguous dates), location (location_ids) info.
-
-#     loaded_data: raw results (a dictionary) from load_data
-#     start_date: the start date for the date column
-#     num_days: the number of days for which the date column needs to be added
-#     """
-#     if start_date is None:
-#         start_date = datetime.strptime(train_start, "%Y-%m-%d")
-#     if end_date is None:
-#         end_date = datetime.strptime(test_end, "%Y-%m-%d")
-#     if train_num_days is None:
-#         train_num_days = len(loaded_data['X_tr'][0])
-#     if test_num_days is None: 
-#         test_num_days = len(loaded_data['X_te'][0])
-
-#     # Extract the individual column names from the nested array
-#     features = [name[0] for name in loaded_data['features'][0]]
-
-#     # Convert the NumPy arrays to Pandas DataFrames
-#     train_dfs = [pd.DataFrame(arr, columns=features) for arr in loaded_data['X_tr'][0]]
-#     test_dfs = [pd.DataFrame(arr, columns=features) for arr in loaded_data['X_te'][0]]
-
-#     # Iterate through the X_tr data frames and add the date and location columns
-#     for d, df in enumerate(train_dfs):
-#         date = start_date + timedelta(days=d)
-#         date_str = date.strftime("%Y-%m-%d")
-
-#         for index, row in df.iterrows():
-#             location_id = loaded_data['location_ids'][index]
-#             df.loc[index, 'Date'] = date_str
-#             df.loc[index, 'Location_ID'] = location_id
-
-#         # Store the modified DataFrame back into the dictionary
-#         loaded_data['X_tr'][0][d] = df
-
-#     # Iterate through the X_te data frames and add the date and location columns
-#     for d, df in reversed(list(enumerate(test_dfs))):
-#         date = end_date - timedelta(days=d)
-#         date_str = date.strftime("%Y-%m-%d")
-
-#         for index, row in df.iterrows():
-#             location_id = loaded_data['location_ids'][index]
-#             df.loc[index, 'Date'] = date_str            
-#             df.loc[index, 'Location_ID'] = int(location_id)
-
-#         # Store the modified DataFrame back into the dictionary
-#         loaded_data['X_te'][0][d] = df
-
-#     return loaded_data
 
 def add_temporal_spatial_cols(
         loaded_data
