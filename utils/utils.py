7c43c04bf0b37b958909a239668b4c0d81a74afb
Yinpu Li <yinpuli@icloud.com>
2023-08-22 20:45:11 -0700

    Add more features for time column.
    Save data to csv files.

--- utils/utils.py
+++ utils/utils.py
@@ -165,10 +165,10 @@ def add_temporal_spatial_cols(
             location_id = loaded_data['location_ids'][index]
             df.loc[index, 'Date'] = date_str
             df.loc[index, 'Location_ID'] = location_id
-            df.loc[index, 'Year'] = date.year
-            df.loc[index, 'Month'] = date.month
-            df.loc[index, 'Week'] = date.isocalendar().week
-            df.loc[index, 'Weekday'] = date.weekday()
+            df.loc[index, 'Year']    = str(int(date.year))
+            df.loc[index, 'Month']   = str(int(date.month))
+            df.loc[index, 'Week']    = str(int(date.isocalendar().week))
+            df.loc[index, 'Weekday'] = str(int(date.weekday()))
 
     # Iterate through the X_te data frames and add the date and location columns
     for d, df in reversed(list(enumerate(test_dfs))):
