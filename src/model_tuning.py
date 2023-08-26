c8daa0a6ea2fe1a7dd8ac9d1c0b0580c700c24fd
Yinpu Li <yinpuli@icloud.com>
2023-08-25 22:27:57 -0700

    Add hyper tuning func.

--- src/model_tuning.py
+++ src/model_tuning.py
@@ -5,6 +5,7 @@ import ast
 from typing import Union
 import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
@@ -13,16 +14,86 @@ from utils.utils     import *
 from utils.constants import *
 from utils.metrics   import *
 
+# Scripts:
 
-def hyperparamter_tuning(
-        X_train: pd.DataFrame
-        , y_train: pd.DataFrame
-        , param_grid: dict
-        , model: Union[xgb.sklearn.XGBRegressor]
-        , scores_func: function
 
+def hyperparameter_tuning(
+        X_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_test: pd.DataFrame,
+        param_grid: dict,
+        model: Union[xgb.sklearn.XGBRegressor, RandomForestRegressor],
+        scoring: function,
+        eval_func: function,
+        file_path: str,
+        cv: int
 ) -> dict:
+    """
+    Perform hyperparameter tuning, prediction, and evaluation for a given model.
+
+    Parameters:
+    X_train (pd.DataFrame): Training features.
+    y_train (pd.DataFrame): Training target values.
+    X_test (pd.DataFrame): Testing features.
+    y_test (pd.DataFrame): Testing target values.
+    param_grid (dict): Grid of hyperparameters for tuning.
+    model (Union[xgb.sklearn.XGBRegressor, RandomForestRegressor]): Initialized model.
+    scoring (function): Scoring function for hyperparameter tuning.
+    eval_func (function): Evaluation function for model performance.
+    file_path (str): File path for saving the best model and results.
+    cv (int): Number of cross-validation folds.
+
+    Returns:
+    dict: Dictionary containing best model information and evaluation metrics.
     
+    Example:
+    X_train = X_train_preprocessed_df
+    y_train = y_train
+    X_test  = X_test_preprocessed
+    y_test = y_test
+    model = xgb.XGBRegressor(objective='reg:squarederror') # RandomForestRegressor()
+    scoring = make_scorer(lambda y_true, y_pred: -mean_squared_error(y_true, y_pred, squared=False))
+    param_grid = {
+        'n_estimators': [50, 100],
+        'max_depth': [None, 3, 5, 10, 20],
+        'min_samples_split': [1, 2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
 
+    eval_func = compute_metrics
+    
+    """
 
-    return {}
+    # Perform hyperparameter tuning using GridSearchCV
+    grid_search = GridSearchCV(model, param_grid, cv=cv, scoring=scoring)
+    grid_search.fit(X_train, y_train)
+
+    # Get the best hyperparameters and best model
+    best_params = grid_search.best_params_
+    best_model = grid_search.best_estimator_
+    best_score = grid_search.best_score_
+
+    # Making predictions on the validation data using the best model
+    y_pred = best_model.predict(X_test)
+
+    # Calculate evaluation metrics
+    eval_metrics = eval_func(y_test['measurement'], y_pred)
+
+    # Get feature importance scores
+    feature_importance = best_model.feature_importances_
+    feature_names = X_train.columns
+    feature_importance_dict = dict(zip(feature_names, feature_importance))
+
+    # Save the results
+    best_model_info = {
+        'best_params': best_params,
+        'best_score': best_score,
+        'feature_importance': feature_importance_dict,
+        'eval_metrics': eval_metrics
+    }
+    
+    # Save the best model and results
+    save_model(file_name=file_path, model=best_model, model_info=best_model_info)
+
+    return best_model_info
