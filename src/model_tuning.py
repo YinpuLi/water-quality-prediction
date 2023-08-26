dfa573ae1c78e204628ace1890ecf1918f3ed3d2
Yinpu Li <yinpuli@icloud.com>
2023-08-25 21:44:22 -0700

    XGBoost metric results done.

---
+++ src/model_tuning.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+import sys
+import ast
+
+# root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
+# sys.path.append(root_dir)
+
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_dir)
