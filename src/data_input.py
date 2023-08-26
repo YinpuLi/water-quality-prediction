0bcc08dd01112e526cb21827882b9b3eb1e23a7b
Yinpu Li <yinpuli@icloud.com>
2023-08-25 21:49:29 -0700

    Save RF best model (temp).

--- src/data_input.py
+++ src/data_input.py
@@ -3,9 +3,6 @@ import os
 import sys
 import ast
 
-# root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
-# sys.path.append(root_dir)
-
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
--- src/model_tuning.py
+++ src/model_tuning.py
@@ -2,10 +2,16 @@ import pandas as pd
 import os
 import sys
 import ast
-
-# root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
-# sys.path.append(root_dir)
-
+from typing import Union
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
+
+
+def hyperparamter_tuning(
+        X_train: pd.DataFrame
+        , y_train: pd.DataFrame
+        , param_grid: dict
+        , model: Union[]
+        , 
+)
