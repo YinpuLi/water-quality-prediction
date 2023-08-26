0566e785bfb7356ebf4f240af4d571f2f7d58119
Yinpu Li <yinpuli@icloud.com>
2023-08-25 21:55:50 -0700

    Initialize model_tuning module.

--- src/model_tuning.py
+++ src/model_tuning.py
@@ -3,15 +3,26 @@ import os
 import sys
 import ast
 from typing import Union
+import xgboost as xgb
+from sklearn.ensemble import RandomForestRegressor
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
+
+from utils.utils     import *
+from utils.constants import *
+from utils.metrics   import *
 
 
 def hyperparamter_tuning(
         X_train: pd.DataFrame
         , y_train: pd.DataFrame
         , param_grid: dict
-        , model: Union[]
-        , 
-)
+        , model: Union[xgb.sklearn.XGBRegressor]
+        , scores_func: function
+
+) -> dict:
+    
+
+
+    return {}
